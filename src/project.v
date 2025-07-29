cf131c6eae21821194c602b50d74373923da6aee
Kishore Netheti <kishorenetheti01@gmail.com>
2025-07-29 17:57:18 +0530

    Updated project.v with latest changes

--- src/project.v
+++ src/project.v
@@ -65,9 +65,9 @@ module PC(
 
   always @(posedge clk or posedge rst) begin
     if (rst)
-      p_c <= 0;
+      p_c <= 16'd0;
     else if (p_c >= 16'd30)  // wrap at last instruction (15*2 = 30)
-      p_c <= 0;
+      p_c <= 16'd0;
     else
       p_c <= pc_next;
   end
@@ -82,7 +82,7 @@ module instruction_memory(
   output wire [15:0] instruction
 );
   reg [15:0] instruction1;
-  reg [15:0] rom [15:0];
+  reg [15:0] rom [0:15];  // Fixed array declaration
   
   initial begin
     rom[0]  = 16'b0000_0001_0010_0011;  // ADD
@@ -175,6 +175,14 @@ module decode(
         rs = instruction_in[7:4];
         rt = instruction_in[3:0];
       end
+
+      default: begin
+        rs = 4'b0000;
+        rt = 4'b0000;
+        rd = 4'b0000;
+        im = 4'b0000;
+        jump = 12'b000000000000;
+      end
     endcase
   end
 endmodule
@@ -187,50 +195,49 @@ module control_unit(
 );
 
   always @(*) begin
+    // Default values
+    RegDst = 1'b0; ALUsrc = 1'b0; MemtoReg = 1'b0; RegWrite = 1'b0;
+    MemWrite = 1'b0; MemRead = 1'b0; jump = 1'b0; ALUOp = 4'b0000;
+    
     case (opcode)
       4'b0000: begin  // ADD
-        RegDst = 1; ALUsrc = 0; MemtoReg = 0; RegWrite = 1;
-        MemWrite = 0; MemRead = 0; jump = 0; ALUOp = 4'b0000;
+        RegDst = 1'b1; ALUsrc = 1'b0; MemtoReg = 1'b0; RegWrite = 1'b1;
+        MemWrite = 1'b0; MemRead = 1'b0; jump = 1'b0; ALUOp = 4'b0000;
       end
 
       4'b0001: begin  // SUB
-        RegDst = 1; ALUsrc = 0; MemtoReg = 0; RegWrite = 1;
-        MemWrite = 0; MemRead = 0; jump = 0; ALUOp = 4'b0001;
+        RegDst = 1'b1; ALUsrc = 1'b0; MemtoReg = 1'b0; RegWrite = 1'b1;
+        MemWrite = 1'b0; MemRead = 1'b0; jump = 1'b0; ALUOp = 4'b0001;
       end
 
       4'b0010: begin  // ADDI
-        RegDst = 0; ALUsrc = 1; MemtoReg = 0; RegWrite = 1;
-        MemWrite = 0; MemRead = 0; jump = 0; ALUOp = 4'b0010;
+        RegDst = 1'b0; ALUsrc = 1'b1; MemtoReg = 1'b0; RegWrite = 1'b1;
+        MemWrite = 1'b0; MemRead = 1'b0; jump = 1'b0; ALUOp = 4'b0010;
       end
 
       4'b0011: begin  // LW
-        RegDst = 0; ALUsrc = 1; MemtoReg = 1; RegWrite = 1;
-        MemWrite = 0; MemRead = 1; jump = 0; ALUOp = 4'b0011;
+        RegDst = 1'b0; ALUsrc = 1'b1; MemtoReg = 1'b1; RegWrite = 1'b1;
+        MemWrite = 1'b0; MemRead = 1'b1; jump = 1'b0; ALUOp = 4'b0011;
       end
 
       4'b0100: begin  // SW
-        RegDst = 0; ALUsrc = 1; MemtoReg = 0; RegWrite = 0;
-        MemWrite = 1; MemRead = 0; jump = 0; ALUOp = 4'b0100;
+        RegDst = 1'b0; ALUsrc = 1'b1; MemtoReg = 1'b0; RegWrite = 1'b0;
+        MemWrite = 1'b1; MemRead = 1'b0; jump = 1'b0; ALUOp = 4'b0100;
       end
 
       4'b0101: begin  // JUMP
-        RegDst = 0; ALUsrc = 0; MemtoReg = 0; RegWrite = 0;
-        MemWrite = 0; MemRead = 0; jump = 1; ALUOp = 4'b0000;
+        RegDst = 1'b0; ALUsrc = 1'b0; MemtoReg = 1'b0; RegWrite = 1'b0;
+        MemWrite = 1'b0; MemRead = 1'b0; jump = 1'b1; ALUOp = 4'b0000;
       end
 
       4'b0110: begin  // XOR
-        RegDst = 1; ALUsrc = 0; MemtoReg = 0; RegWrite = 1;
-        MemWrite = 0; MemRead = 0; jump = 0; ALUOp = 4'b0110;
+        RegDst = 1'b1; ALUsrc = 1'b0; MemtoReg = 1'b0; RegWrite = 1'b1;
+        MemWrite = 1'b0; MemRead = 1'b0; jump = 1'b0; ALUOp = 4'b0110;
       end
 
       4'b0111: begin  // OR
-        RegDst = 1; ALUsrc = 0; MemtoReg = 0; RegWrite = 1;
-        MemWrite = 0; MemRead = 0; jump = 0; ALUOp = 4'b0111;
-      end
-
-      default: begin
-        RegDst = 0; ALUsrc = 0; MemtoReg = 0; RegWrite = 0;
-        MemWrite = 0; MemRead = 0; jump = 0; ALUOp = 4'b0000;
+        RegDst = 1'b1; ALUsrc = 1'b0; MemtoReg = 1'b0; RegWrite = 1'b1;
+        MemWrite = 1'b0; MemRead = 1'b0; jump = 1'b0; ALUOp = 4'b0111;
       end
     endcase
   end
@@ -271,7 +278,8 @@ module data_memory(
   output reg [15:0] read_data
 );
 
-  reg [15:0] mem [63:0]; // 64 words of memory
+  reg [15:0] mem [0:63]; // Fixed array declaration - 64 words of memory
+  integer i;
 
   initial begin
     // Initialize memory with some test values
@@ -280,7 +288,7 @@ module data_memory(
     mem[2] = 16'h9ABC;
     mem[3] = 16'hDEF0;
     // Initialize rest to 0
-    for (integer i = 4; i < 64; i = i + 1) begin
+    for (i = 4; i < 64; i = i + 1) begin
       mem[i] = 16'h0000;
     end
   end
@@ -363,7 +371,8 @@ module mips_single_cycle(
   );
 
   // Register File
-  reg [15:0] reg_file [15:0];
+  reg [15:0] reg_file [0:15];  // Fixed array declaration
+  integer j;
   
   // Initialize register file
   initial begin
@@ -373,8 +382,8 @@ module mips_single_cycle(
     reg_file[3] = 16'h0003;  // $v1
     reg_file[4] = 16'h0004;  // $a0
     reg_file[5] = 16'h0005;  // $a1
-    for (integer i = 6; i < 16; i = i + 1) begin
-      reg_file[i] = 16'h0000;
+    for (j = 6; j < 16; j = j + 1) begin
+      reg_file[j] = 16'h0000;
     end
   end
 
@@ -415,6 +424,7 @@ module mips_single_cycle(
   assign write_data_final = MemtoReg ? mem_read_data : ALU_out;
 
   // Register file write
+  integer k;
   always @(posedge clk) begin
     if (rst) begin
       reg_file[0] <= 16'h0000;  // $zero always 0
@@ -423,10 +433,10 @@ module mips_single_cycle(
       reg_file[3] <= 16'h0003;
       reg_file[4] <= 16'h0004;
       reg_file[5] <= 16'h0005;
-      for (integer i = 6; i < 16; i = i + 1) begin
-        reg_file[i] <= 16'h0000;
+      for (k = 6; k < 16; k = k + 1) begin
+        reg_file[k] <= 16'h0000;
       end
-    end else if (RegWrite && write_reg != 0) begin // Don't write to $zero
+    end else if (RegWrite && write_reg != 4'b0000) begin // Don't write to $zero
       reg_file[write_reg] <= write_data_final;
     end
   end
